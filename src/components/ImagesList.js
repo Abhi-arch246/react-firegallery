c707c3bad378f0da239929d929b20ed217ce8dcc
Abhi-arch246 <abhishekreddy235@gmail.com>
2021-04-28 14:09:36 +0530

    img alt text

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -8,7 +8,7 @@ function ImagesList() {
         <div>
             {docs && docs.map(doc => (
                 <div className="single-img row d-inline p-1" key={doc.id}>
-                    <img className="col-md-4 rounded" src={doc.url} width="80" height="350" />
+                    <img className="col-md-4 rounded" src={doc.url} alt="img" width="80" height="350" />
 
                 </div>
 
